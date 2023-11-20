a026b3f9706eb5543db22cf5f8375fdcc4bc37ab
Søs Wind <s_swind@hotmail.com>
2023-11-20 11:23:28 +0100

    Login knap hvid opsat korrekt

--- Navbar.jsx
+++ Navbar.jsx
@@ -3,13 +3,15 @@ import React from "react"
 
 export default function Navbar() {
     return (
-        <nav>
-            <ul className="nav-links">
+        <nav className="nav-links">
+            <ul className="left">
                 <li><a href="/">All Seafares</a></li>
                 <li><a href="/">About</a></li>
                 <li><a href="/">Good To Know</a></li>
                 <li><a href="/">Sign Up</a></li>
-                <li className="login-button"><a href="/">Login</a></li>
+            </ul>
+            <ul>
+                <li><a className="login-button" href="/">Login</a></li>
             </ul>
         </nav>
     )
