aa9ca098415bc997e7e56457edf86a4d911ba84e
Søs Wind <s_swind@hotmail.com>
2023-11-21 12:22:41 +0100

    Landingpage oprettet mobil version

---
+++ Landingpage.jsx
@@ -0,0 +1,40 @@
+import React from "react"
+
+
+export default function Landingpage() {
+    return (
+        <section>
+            <img className="boat-image" src="../src/images/boat.png"></img>
+            <h2>Create a free profile, and put together the perfect crew for your seafare</h2>
+            <p className="land-paragraph">With a free captain profile at SailMore, you'll gain access to our crew exchange, where you get
+                an easy and clear overview, letting you find both new and experienced crew members, that match your
+                personality and experience requirements for your seafares.
+            </p>
+            <ul>
+                <li>Skip the struggle of going through an abudance of crew applications for each cruise.</li>
+                <li>Explore the crew profiles, and make your own choice of exactly who fits your wishes and requirements for the cruises.</li>
+                <li>Give the crew a serious overview of scopes and activities on your cruises.</li>
+            </ul>
+            <button className="crew-profiles-button" type="button">Browse crew profiles</button>
+            <img className="fordaek-image" src="../src/images/fordaek-bog.png"></img>
+            <h2>Set the sail for far-away coasts with experienced captains!</h2>
+            <p>Are you dreaming about crossing the Atlantic Sea, stepping upon deserted islands in Polynesia, or maybe just enjoy a quiet sunset at Kattegat?
+                Do you want to experience the world in a social, shared-economy and ecological way of travel? Become a crew through SailMore!
+            </p>
+            <ul>
+                <li>Get in contact with experienced captains, that are organizing cruises in Denmark and internationally.</li>
+                <li>Find cruises, that match your dreams of adventure and activities.</li>
+                <li>Get sailing experience and personal development experiences, that you'll never forget.</li>
+                <li>There's room for everyone - Whether you are an experienced sailor, or a complete land-dweller.</li>
+            </ul>
+            <button className="captain-button" type="button">Browse captains profiles</button>
+            <img className="section-image" src="../src/images/section-image.png"></img>
+            <h2 className="h2-in-image">Guide to long-distance sailing and other seafares!</h2>
+            <p className="paragraph-in-image">Before you set of on a cruise, it's a good idea to long-distance sailing. Whether you are used to acting as a guest or a captain on a sailboat or not, it is good
+                to know which rules and conditions apply before and during long-distance sailing and sailing trips in general.
+            </p>
+            <button className="long-distance-button" type="button">SailMores guide to long-distance sailing</button>
+            </section>
+    )
+
+}
--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import NavbarTop from './NavbarTop'
 import Navbar from '../Navbar'
+import Header from '../Header'
+import Landingpage from '../Landingpage'
+import CarrouselLandingpage from '../CarrouselLandingpage'
 
 
 function App() {
@@ -18,21 +21,19 @@ function App() {
     <div>
       <Navbar />
     </div>
+  
+    <div>
+      <Header />  
+    </div>
 
-     
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Landingpage />
+    </div>
+
+    <div>
+      <CarrouselLandingpage/>
+    </div>
+  </>
   )
 }
 
