e57730b8f9ec672cff696b5a24a0b46af5a84950
tinaemmik <98392184+tinaemmik@users.noreply.github.com>
2023-11-27 10:55:43 +0100

    CSS styling added

--- src/NavbarBottom.jsx
+++ src/NavbarBottom.jsx
@@ -7,31 +7,31 @@ export default function NavbarBottom() {
                 <img src="../src/images/hvid-sailmore-logo.svg" className="footer-img"></img>
             </div>
             <div className="footer-info">
-                <h3>Contact</h3>
                 <ul>
+                    <h3>Contact</h3>
                     <li>Phone: 0045 66 66 66 66</li>
                     <li>E-mail: sailmore@mail.dk</li>
                     <li>CVR: 456789</li>
                 </ul>
             </div>
             <div className="footer-address">
-                <h3>Address</h3>
                 <ul>
+                    <h3>Address</h3>
                     <li>Sailvej 67</li>
                     <li>8000 Aarhus C</li>
                     <li>Denmark</li>
                 </ul>
             </div>
             <div className="footer-links">
-                <h3>Important links</h3>
                 <ul>
+                    <h3>Important links</h3>
                     <li><a href="/">Create Profile</a></li>
                     <li><a href="/">Login</a></li>
                     <li><a href="/">All seafares</a></li>
                 </ul>
             </div>
         </div><div className="footer-bottom">
-                <p>© 2023 Sailmore. All rights reserved.</p>
+                <p>2023 | SailMore</p>
             </div></>
     )
 }
