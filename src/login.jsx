3dd1a9749398c0ca9f3077094a816f74e9f87b09
Tine <111304307+TineMSPetersen@users.noreply.github.com>
2023-11-29 19:26:01 +0100

    Update login.jsx

--- src/login.jsx
+++ src/login.jsx
@@ -5,6 +5,7 @@ import './App.css'
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const history = useHistory();
     
     const handleSubmit = async (e) => {
         e.preventDefault();
