c6f1c6e8bfae1f8734aad83e46e166490ee6f7db
TineMSPetersen <tinemeretesikjaerpetersen@gmail.com>
2023-12-02 12:21:10 +0100

    Change header to splashscreen

---
+++ Splashscreen.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Splashscreen() {
+    return (
+    <section>
+    <img src="../src/images/header-image.png" className="header-image"></img>
+    <h1 className="title-header">Sail with experienced captains and adventurous crew on course for near and faraway coasts</h1>
+    <p className="paragraph-header">Connects adventurous crew with experienced captains for long-distance sailing</p>
+    <button className="header-button" type="button">Create profile</button>
+    </section>
+
+    )
+}
