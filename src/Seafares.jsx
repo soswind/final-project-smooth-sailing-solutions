0bb6cc33cddddd746d698b413346940370d86b53
Tine <111304307+TineMSPetersen@users.noreply.github.com>
2023-12-03 14:12:08 +0100

    Apply responsive grid

--- src/Seafares.jsx
+++ src/Seafares.jsx
@@ -7,12 +7,11 @@ export default function Seafares() {
 
 
   return (
-    <>
-    <Header />
+    <div className="seafareItems">
     <SeafareItem title="Togt 26. Fransk Polynesien" start_date="3. December" end_date="15. January" destination="Fransk Polynesien" seafare_area="Stillehavet" price="43.000 DKK" captain="Skipper Bent" />
     <SeafareItem />
     <SeafareItem />
     <SeafareItem />
-    </>
+    </div>
   )
 }
