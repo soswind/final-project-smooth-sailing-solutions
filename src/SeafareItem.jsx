e48fb165d797cbd2c4580ad3b5b3593e245d287f
Tine <111304307+TineMSPetersen@users.noreply.github.com>
2023-12-03 14:07:48 +0100

    Styling for SeafareItem

--- src/SeafareItem.jsx
+++ src/SeafareItem.jsx
@@ -1,9 +1,9 @@
 export default function SeafareItem(props) {
   return (
-    <>
-    <h2>{props.title}</h2>
-    <p>{props.start_date} - {props.end_date} | {props.destination} ({props.seafare_area}) | {props.price}</p>
-    <h3>Captain: {props.captain}</h3>
-    </>
+    <article className="seafareItem">
+      <h2 className="seafareItem_title">{props.title}</h2>
+      <p className="seafareItem_details">{props.start_date} - {props.end_date} | {props.destination} ({props.seafare_area})</p>
+      <h3 className="seafareItem_captain">Captain: {props.captain}</h3>
+    </article>
   )
 }
