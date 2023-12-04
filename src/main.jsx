d167efaa0547515510d08c103dbd71d0e542cadf
Søs Wind <s_swind@hotmail.com>
2023-12-04 10:30:03 +0100

    Update signup form
    Signup form css done

--- src/main.jsx
+++ src/main.jsx
@@ -5,21 +5,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './App.css'
-import Chat from '../Chat.jsx'
-import Login from './Login.jsx'
-import SignUpForm from '../Signup.jsx'
-import NavbarTop from './NavbarTop.jsx'
-import MobileMenu from './MobileMenu.jsx'
-import Landingpage from '../Landingpage.jsx'
-import Header from '../Splashscreen.jsx'
-import Navbar from '../Navbar.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NavbarTop/>
-    <MobileMenu/>
-    <Header/>
-    <SignUpForm/>
-    <Landingpage/>
+    <App />
   </React.StrictMode>
 )
