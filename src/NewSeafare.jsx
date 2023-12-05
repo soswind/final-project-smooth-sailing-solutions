4b9d2350fcefc5cbb7da0b54bb628cbeb7b371a1
Tine <111304307+TineMSPetersen@users.noreply.github.com>
2023-12-05 19:43:14 +0100

    Create NewSeafare component

---
+++ src/NewSeafare.jsx
@@ -0,0 +1,11 @@
+export default function NewSeafare() {
+  return(
+    <>
+    <form action="">
+      
+    </form>
+    </>
+  )
+}
+
+// Title, destination, seafare_description, start_date, end_date, start_time end_time, price, seafare_area 
