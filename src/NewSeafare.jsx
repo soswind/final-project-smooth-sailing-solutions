539650e5b01360d32f081df5c5f872da091b7df6
Tine <111304307+TineMSPetersen@users.noreply.github.com>
2023-12-06 15:02:06 +0100

    UI fixes

--- src/NewSeafare.jsx
+++ src/NewSeafare.jsx
@@ -1,14 +1,14 @@
 export default function NewSeafare() {
   return(
     <>
-    <h2>Hello</h2>
+    <h2>Post new seafare</h2>
     <form method="POST" className="seafare-form" action="/api/create_seafare.php">
     
     <label className="form-label" htmlFor="title">Title:</label>
-    <input className="single" type="text" name="title" id="title" />
+    <input className="single" type="text" name="title" id="title" placeholder="Example: 120 day seafare on luxus boat" />
 
     <label className="form-label" htmlFor="description">Description:</label>
-    <textarea name="description" id="description" columns="10" rows="5"></textarea>
+    <textarea name="description" id="description" columns="10" rows="5" placeholder="What makes your seafare special?"></textarea>
 
     <div className="form-grid">
         <div><label className="form-label" htmlFor="seafare_area">Area:</label>
@@ -32,7 +32,7 @@ export default function NewSeafare() {
 
         <div>
           <label className="form-label" htmlFor="destination">Destination:</label>
-          <input type="text" name="destination" id="destination" />
+          <input type="text" name="destination" id="destination" placeholder="Example: Japan" />
         </div>
 
 
@@ -58,9 +58,9 @@ export default function NewSeafare() {
     </div>
 
     <label className="form-label" htmlFor="price">Price:</label>
-    <input className="single" type="number" name="price" id="price" />
+    <input className="single" type="number" name="price" id="price" placeholder="Prices listed in DKK"/>
 
-<button type="submit">Submit</button>
+<button className="button button--small" type="submit">Submit</button>
     
     </form>
     </>
