77faee50628fdaaeab3c4bfabcae4d112218c605
Tine <111304307+TineMSPetersen@users.noreply.github.com>
2023-12-08 16:47:14 +0100

    Import globe picture

--- src/DashboardSeafare.jsx
+++ src/DashboardSeafare.jsx
@@ -1,5 +1,7 @@
+import globe from './images/globe.png'
+
 export default function DashboardSeafare(props) {
   return (<section className="seafare">
-    <img src="" alt="" /><p>{props.seafare_name}</p> <p>D. {props.start_date} - {props.end_date}</p>
+    <img src={globe} /><p>{props.seafare_name}</p> <p>D. {props.start_date} - {props.end_date}</p>
   </section>)
 }
