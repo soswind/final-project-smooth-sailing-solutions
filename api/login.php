3d7379fb0360ab181818db9caf6970479c51b602
Tine <111304307+TineMSPetersen@users.noreply.github.com>
2023-12-08 13:42:56 +0100

    Make login check database
    Login now checks if there's a matching set in the database. It also stores the user info in session

--- api/login.php
+++ api/login.php
@@ -1,12 +1,22 @@
 <?php
 include './mysql.php';
 header('Content-Type: application/json');
+session_start();
 
 // Get data from the request body
 $data = json_decode(file_get_contents('php://input'), true);
 
+$query = "SELECT * FROM crew_profile WHERE email = ? AND password = ?";
+$stmt = $mySQL->prepare($query);
+$stmt->bind_param("ss", $data['email'], $data['password']);
+$stmt->execute();
+$result = $stmt->get_result();
+
 // Example: Check username and password (replace with your authentication logic)
-if ($data['email'] === 'example_user@gmail.com' && $data['password'] === 'example_password') {
+if ($result->num_rows === 1) {
+    $user = $result->fetch_assoc();
+    $_SESSION['user'] = $user;
+    
     http_response_code(200);
     echo json_encode(['message' => 'Login successful']);
 } else {
