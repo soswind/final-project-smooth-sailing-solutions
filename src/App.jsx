5138ad276ebed8689c5cf00b9b6583058a4c0bbd
Søs Wind <s_swind@hotmail.com>
2023-12-09 13:06:14 +0100

    Find Gaster ny database tabel
    Opsat ny database tabel og opdateret items, så de alle vises i browseren.

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,23 @@ import { useState } from 'react'
 import './App.css'
 import Header from './Header'
 import { Routes, Route } from 'react-router-dom';
-import Header from './Header'
 import Login from './login'
 import Dashboard from './Dashboard'
+import FindGaster from './FindGaster';
 
 
 
 function App() {
   return (
     <>
+    
     <Header />
     <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
+    <FindGaster />
+    
   </>
 
   )
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -6,10 +6,10 @@ const Profile = ({ data }) => {
     <div className='outer-profile'>
        {data.map(item => (
     <div className="profile" key={item.id}>
-      <h2>{item.first_name} {item.last_name}</h2>
-      <p>Age: {item.age}</p>
-      <p>ID: {item.id}</p>
-      <p>Image: {item.image_url}</p>
+      <h2>{item.crew_first_name} {item.crew_last_name}</h2>
+      <p>City: {item.city}</p>
+      <p>Zip Code: {item.zip_code}</p>
+      <p>Birthday: {item.birthday}</p>
       <p>Description: {item.profile_text}</p>
     </div>
   ))
